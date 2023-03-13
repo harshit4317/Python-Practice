064dce571db61d1de52b423743d9923294ded0c4
Harshit <harshit4317@gmail.com>
2023-03-14 00:41:40 +0530

    Added new files

--- KBC_Project.py
+++ KBC_Project.py
@@ -1,9 +1,19 @@
-#displying welcome message
-print("Welcome To KBC")
-questions=["ques_1","ques_2","ques_3"]
-opt_ques_1=["a","b","c","d"]
-length=len(questions)
-for i in questions:
-    match i:
-        case 1:
-            print(f"Your Question is:{i}")
+questions={"ques1":"hello1","ques2":"hello2","ques3":"hello3","ques4":"hello4"}
+options={"ques1":["a","b","c","d"],"ques2":["a","b","c","d"],"ques3":["a","b","c","d"],"ques4":["a","b","c","d"]}
+answers={"ques1":"a","ques2":"c","ques3":"d","ques4":"a",}
+
+def KBC():
+    print("Welcome To KBC")
+    for i in questions:
+        print("Your Question is: ",questions[i])
+        print(options[i])
+        a=input("Enter your answer: \n")
+        if a==(answers[i]):
+            print("You won a Jackpot......")
+        else:
+            print("You lost the game")
+            break
+    print("Thank You for playing the game")
+
+
+KBC()
--- recursions.py
+++ recursions.py
@@ -2,9 +2,31 @@
 
 
 # example--- factorial of a number
-def fact(n):
-    if(n==0 or n==1):
-        return 1
-    else:
-        return(n*fact(n-1))
-print(fact(5))
+# def fact(n):
+#     if(n==0 or n==1):
+#         return 1
+#     else:
+#         return(n*fact(n-1))
+# print(fact(5))
+# n=int(input("Enter the number terms: "))
+
+
+
+#fibonacci series
+def fibonacci(n):
+	if n<=1:
+		return n
+	else:
+		return(fibonacci(n-1) + fibonacci(n-2))
+
+n = int(input('Enter a number: '))
+
+fibo_series = []
+
+for i in range(0,n):
+	fibo_series.append(fibonacci(i))
+	
+print(fibo_series)
+
+# f0=0 f1=1
+# fn=fn-1+fn-2
